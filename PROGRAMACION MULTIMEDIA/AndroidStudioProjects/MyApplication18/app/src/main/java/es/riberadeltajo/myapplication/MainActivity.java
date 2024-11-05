834f20ad1f0a430396453f8f47efca9770a16f24
Alex <alejandrosanchezgil7@gmail.com>
2024-11-05 15:02:03 +0100

    Actualización

--- PROGRAMACION MULTIMEDIA/AndroidStudioProjects/MyApplication18/app/src/main/java/es/riberadeltajo/myapplication/MainActivity.java
+++ PROGRAMACION MULTIMEDIA/AndroidStudioProjects/MyApplication18/app/src/main/java/es/riberadeltajo/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package es.riberadeltajo.myapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,33 +14,34 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private ActivityResultLauncher<Intent> launcher;
 
-    public final int SELECCION_PROVINCIA=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b=findViewById(R.id.button);
+
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        String retorno = result.getData().getStringExtra("SELECCION");
+                        Toast.makeText(this, "El usuario seleccionó " + retorno, Toast.LENGTH_LONG).show();
+                        TextView t = findViewById(R.id.textView);
+                        t.setText(retorno);
+                    }
+                }
+        );
+        Button b = findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //abrir una segunda actividad!!! Actividad es un proceso distinto
-                Intent i=new Intent(getApplicationContext(),SecondActivity.class);
-                startActivityForResult(i,1);
+                Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+                launcher.launch(i);
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==SELECCION_PROVINCIA)
-            if(resultCode==RESULT_OK){
-                //procesar datos devueltos (ver qué provincia se ha seleccionado)
-                String retorno=data.getExtras().getString("SELECCION");
-                Toast.makeText(this,"El usuario seleccionó "+retorno,Toast.LENGTH_LONG).show();
-                TextView t=findViewById(R.id.textView);
-                t.setText(retorno);
-            }
-    }
 }
