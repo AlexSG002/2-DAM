b0a1ebeed12d3247322cb4bbb52283f5b5dd43e4
Alejandro <alejandrosanchezgil7@gmail.com>
2025-02-12 21:18:01 +0100

    PMDM y AD

---
+++ PROGRAMACION MULTIMEDIA/SEGUNDO TRIMESTRE/UT6/explicacionvideojuego/app/src/main/java/com/example/explicacionvideojuego/BucleJuego.java
@@ -0,0 +1,70 @@
+package com.example.explicacionvideojuego;
+
+import static android.content.ContentValues.TAG;
+
+import android.graphics.Canvas;
+import android.util.Log;
+import android.view.SurfaceHolder;
+
+public class BucleJuego extends Thread {
+    private EboraJuego juego;
+
+    private SurfaceHolder surfaceHolder;
+
+    private boolean juegoEnEjecucion = true;
+
+    public final static int MAX_FPS = 30;
+
+    public final static int TIEMPO_FRAME = 1000/ MAX_FPS;
+
+    public final static int MAX_FRAMES_SALTADOS = 5;
+
+    BucleJuego(SurfaceHolder sh, EboraJuego s){
+        juego = s;
+        surfaceHolder = sh;
+    }
+
+    public void run(){
+        Canvas canvas;
+        Log.d(TAG, "Comienza el GameLoop");
+        long tiempoComienzo;
+        long tiempoDiferencia;
+        long tiempoDormir;
+        long framesASaltar;
+        while(juegoEnEjecucion){
+            canvas = null;
+            try{
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder){
+                    tiempoComienzo = System.currentTimeMillis();
+                    framesASaltar = 0;
+                    juego.actualizar();
+                    juego.renderizar(canvas);
+                    tiempoDiferencia = System.currentTimeMillis() - tiempoComienzo;
+                    tiempoDormir = (int) (TIEMPO_FRAME - tiempoDiferencia);
+                    if(tiempoDormir > 0){
+                        try{
+                            Thread.sleep(tiempoDormir);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    while (tiempoDormir < 0 && framesASaltar > MAX_FRAMES_SALTADOS){
+                        juego.actualizar();
+                        tiempoDormir += TIEMPO_FRAME;
+                        framesASaltar++;
+                    }
+                }
+            }finally {
+                if(canvas != null){
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+        }Log.d(TAG, "Nueva iteración");
+    }
+
+    public void fin(){
+        juegoEnEjecucion = false;
+    }
+
+}
---
+++ PROGRAMACION MULTIMEDIA/SEGUNDO TRIMESTRE/UT6/explicacionvideojuego/app/src/main/java/com/example/explicacionvideojuego/EboraJuego.java
@@ -0,0 +1,127 @@
+package com.example.explicacionvideojuego;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EboraJuego extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+
+    public int maxX = 0;
+
+    public int maxY = 0;
+
+    Bitmap mapa;
+
+    private int mapa_h, mapa_w, dest_mapaY;
+
+    private float pos_inicial_mapa = 0;
+
+    private int contadorFrames = 0;
+
+    private static final int textoInicialx = 50;
+
+    private static final int textoInicialy = 20;
+
+    private BucleJuego bucle;
+
+    private float desplazamientoX = 0;
+    private float velocidadX = 5;
+
+    private float deltaT;
+
+    private SurfaceHolder holder;
+    AppCompatActivity miContexto;
+
+    public EboraJuego(AppCompatActivity context) {
+        super(context);
+        holder = getHolder();
+        holder.addCallback(this);
+        miContexto = context;
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+        getHolder().addCallback(this);
+        Canvas c = holder.lockCanvas();
+        maxX = c.getWidth();
+        maxY = c.getHeight();
+        holder.unlockCanvasAndPost(c);
+        mapa = BitmapFactory.decodeResource(getResources(), R.drawable.mapamario);
+        mapa_h = mapa.getHeight();
+        mapa_w = mapa.getWidth();
+        dest_mapaY = (maxY - mapa_h) / 2;
+        deltaT = 1f / BucleJuego.MAX_FPS;
+
+        bucle = new BucleJuego(getHolder(), this);
+        setFocusable(true);
+        bucle.start();
+
+    }
+
+    public void renderizar(Canvas canvas) {
+        if (canvas != null) {
+            Paint myPaint = new Paint();
+            myPaint.setStyle(Paint.Style.STROKE);
+            canvas.drawColor(Color.RED);
+
+            canvas.save();
+
+
+            int desplazamientoModulo = (int) (desplazamientoX % mapa_w);
+            if (desplazamientoModulo < 0) {
+                desplazamientoModulo += mapa_w;
+
+            }
+
+
+            canvas.drawBitmap(mapa, -desplazamientoModulo, dest_mapaY, null);
+
+            canvas.drawBitmap(mapa, mapa_w - desplazamientoModulo, dest_mapaY, null);
+
+
+
+            myPaint.setStyle(Paint.Style.FILL);
+            myPaint.setTextSize(40);
+
+
+
+            Rect textBounds = new Rect();
+            myPaint.getTextBounds("Frames ejecutados", 0, 1, textBounds);
+            canvas.drawText("Frames ejecutados: " + contadorFrames, textoInicialx,
+                    textoInicialy + textBounds.height(), myPaint);
+        }
+    }
+
+    public void actualizar() {
+        contadorFrames++;
+        desplazamientoX += velocidadX;
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
